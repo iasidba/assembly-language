42c566b8178da230ef5878fd61535ac73710bf94
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-07-25 14:29:07 -0700

    hello world

--- hello.c
+++ hello.c
@@ -1,11 +1,20 @@
-/**
- * Author:Abdi
- * file:hello.c
- */
-#include <stdio.h>
+/* code about prompt to i/o string */
+#include<stdio.h>
 
-int main(void)
+int main()
 {
-	printf ("Hello, World !\n");
-	return 0;
+    int age = 10;
+    char initial = 'A';
+    float weight = 25.5;
+
+    printf("age is \t %d\n",age);
+    printf("initial is \t %c\n",initial);
+    printf("weight is \t %f\n",weight);
+
+    //or we can print everything on the same line
+
+    printf("age=%d \t initial=%c \t weight=%f\n",age,initial,weight);
+
+    return 0;
 }
+        
