ce9d0f6aeb0816b316d64fafc38b1748982248a1
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-07-29 13:14:27 -0700

    prints numbers up to n

---
+++ 8_prt-num.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - prt numbers up to n
+ *Return: 0 success
+ */
+int main(void)
+{
+	int n, i
+
+	printf("Enter a number: ");
+	scanf("%d", &n)
+
+	if(n > 0)
+	{
+		printf("Numbers up to %d is\n", n)
+
+		for(i = 1; i <= n; i++)
+		{
+			printf("%d" , i);
+			if(i % 10 = 0)
+			{
+				printf("\n");
+			}
+		}
+	}
+	else
+	{
+		printf("Erro:pls provide valid number");
+	}
+	return 0;
+}
+
