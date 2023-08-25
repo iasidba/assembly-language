8d401db6ac810e136ccf254d97625bcd571a212f
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 12:00:59 -0700

    print alphabets in reversed

---
+++ Variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+ * file:7-print_tebahpla.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - print alphabets lowercase in reversed.
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
