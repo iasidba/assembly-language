dac4640713a4d35f8dfce771835c56b67d3f51d9
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 12:37:03 -0700

    possible d/t combination of three digit then comma and space in ascending

---
+++ Variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/**
+ * file:101-print_comb4.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+  * main - prints all possible different combinations of three digits
+  *             comma followed by a space, in ascending order.
+  * Return: Always 0(Success)
+  */
+ int main(void)
+ {
+	 int c, i, k;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = c + 1; i <= '9'; i++)
+		{
+			for (k = i + 1; k <= '9'; k++)
+			{
+				putchar(c);
+				putchar(i);
+				putchar(k);
+				if (c != '7')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
