b193fdd40527c99eea2381d1287b7e8e9b06bb18
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 11:22:49 -0700

    print alphabets in lowercase and uppercase

---
+++ Variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+/**
+ * file:3-print_alphabets.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase and uppercase
+ *
+ * Return:Always 0(success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}	
+	putchar('\n');
+	return (0);
+}
