ae0d549a6a36eea9ea972e2d3b19fb1aab97f9d9
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 12:06:46 -0700

    prints all num of base 16 in lowercase

---
+++ Variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+/**
+ * file:8-print_base16.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase,in a new line
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+	putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
