d4c771b4e73ad97662ec6a4b5b54066cc7a63db2
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 11:56:28 -0700

    prints all single digit numof base 10 from 0 use only putchar

---
+++ Variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+/**
+ * file:6-print_numberz.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0 in newline
+ *          only use putchar.
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
