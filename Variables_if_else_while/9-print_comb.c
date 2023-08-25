1a4420ef1554c8573ea0630b9feae5b6911b43c2
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 12:12:42 -0700

    possible comb of single digit num

---
+++ Variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+/**
+ * file:9-print_comb.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
