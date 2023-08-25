5f4f7a8cc8372ea828f4b10b4fe82cbbe3cee713
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-25 11:48:15 -0700

    print all single digit num of base 10 from zero

---
+++ Variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/**
+ * file:5-print_numbers.c
+ * Author:Abdi Heydar
+ */
+
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0 in newline
+ *
+ * Return:Always 0(success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= '9')
+	{
+		num++;
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
