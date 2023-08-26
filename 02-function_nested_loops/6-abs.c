73128a77b2786004e4d4486b7482eb0a3e5b6950
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-08-26 16:05:39 -0700

    function to compute absolute value of int

---
+++ 02-function_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs -function to compute absolute value of int
+ *@i:the int to be computed
+ *Return:absolute value of numbers
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
