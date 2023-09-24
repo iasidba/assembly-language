ef418f891a9087bb6d59554ac5479cf71be8cc69
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-09-24 13:45:37 -0700

    fizzbuzz

---
+++ 03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print num from 1 - 100 ,but instead of mult,
+ *       of 3 Fizz, mult of 5 Buzz, and Mult of
+ *        both 3 and 5 FizzBuzz.
+ *Return:0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz");
+		}
+
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf("Buzz");
+		}
+
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
