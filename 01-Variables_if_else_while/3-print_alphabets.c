507a3a090f803ce16e873f83cad3e411dc403a35
ABDI IBRAHIM <abdisaibrahim143@gmail.com>
2023-09-25 12:03:13 -0700

    prints alphabet in lowercase and in uppercase

--- 01-Variables_if_else_while/3-print_alphabets.c
+++ 01-Variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,12 @@
  * file:3-print_alphabets.c
  * Author:Abdi Heydar
  */
-
 #include <stdio.h>
 /**
  * main - print alphabets in lowercase and uppercase
  *
  * Return:Always 0(success)
  */
-
 int main(void)
 {
 	int c;
